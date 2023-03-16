9447f8e4f4cb46aa246f8f108cdc7727f07e212c
namleenk <knamleen@gmail.com>
2023-03-15 22:17:17 -0400

    Implemented the controller with getters and a setter for the music. Changed the music field to be string | undefined b/c when a player joins the area there will be no music playing

--- frontend/src/classes/DanceAreaController.ts
+++ frontend/src/classes/DanceAreaController.ts
@@ -1,42 +1,20 @@
 import { EventEmitter } from 'events';
 import { useEffect, useState } from 'react';
 import TypedEventEmitter from 'typed-emitter';
-// import { DanceArea as DanceAreaModel } from '../types/CoveyTownSocket';
+import { DanceArea, KeySequence } from '../types/CoveyTownSocket';
+import { DanceArea as DanceAreaModel } from '../types/CoveyTownSocket';
 
 /**
  * The events that a DanceAreaController can emit
  */
 export type DanceAreaEvents = {
-  // players points changed
-  /**
-   * A pointsChanged event indicates that the points for a certain player have been
-   * changed.
-   * @param playerId the id of the player whose points changed
-   * @param points the points being added to the player's current points
-   */
-  pointsChanged: (playerId: string, points: number) => void;
+  // The only client to server communication would be changing the music
 
-  // key sequence --> should notify the players of the new sequnce
-  /**
-   * A newKeySequence event indicates that there is a new key sequence for the
-   * players to follow.
-   * @param sequence the new sequence for the players to follow
-   * @returns a sequence of numbers representing different keys
-   */
-  newKeySequence: (sequence: number[]) => number[];
-
-  // music change --> maybe we can notify the players of the music with a
-  // toast message or string of some form
   /**
    * A musicChanged event indicates that a new song is playing in the DanceArea.
    * @param music the title of the new song
    */
-  musicChanged: (music: string) => void;
-
-  /*new round --> I feel like we can use numbers to keep track of the rounds
-    and just increase the number when there is a new round. If no one is in the
-    area, we can reset the round back to 0.
-    newRound: (roundId: string) => void*/
+  musicChanged: (music: string | undefined) => void;
 };
 
 /**
@@ -49,3 +27,91 @@ export type DanceAreaEvents = {
  * sequences and music in this browser window and emits updates when the state is
  * updated @see DanceAreaEvents
  */
+export default class DanceAreaController extends (EventEmitter as new () => TypedEventEmitter<DanceAreaEvents>) {
+  private _model: DanceAreaModel;
+
+  /**
+   * Constructs a new DanceAreaController, initialized with the state of the
+   * provided danceAreaModel.
+   *
+   * @param danceAreaModel The dance area model that this controller should represent
+   */
+  constructor(danceAreaModel: DanceAreaModel) {
+    super();
+    this._model = danceAreaModel;
+  }
+
+  /**
+   * The ID of the dance area represented by this dance area controller
+   * This property is read-only: once a DanceAreaController is created, it will always
+   * be tied to the same ID.
+   */
+  public get id(): string {
+    return this._model.id;
+  }
+
+  /**
+   * The music of the dance area, or undefined when the first player joins the area.
+   */
+  public get music(): string | undefined {
+    return this._model.music;
+  }
+
+  /**
+   * If the music changes, set it to the new music and emit an update.
+   */
+  public set music(music: string | undefined) {
+    if (this._model.music !== music) {
+      this._model.music = music;
+      this.emit('musicChanged', music);
+    }
+  }
+
+  /**
+   * The round that is ongoing in the dance area.
+   */
+  public get roundId(): string {
+    return this._model.roundId;
+  }
+
+  /**
+   * The current key sequence that the player must follow.
+   */
+  public get keySequence(): KeySequence {
+    return this._model.keySequence;
+  }
+
+  /**
+   * The duration (in seconds) of the current round.
+   */
+  public get duration(): number {
+    return this._model.duration;
+  }
+
+  /**
+   * The amount points of each player currently in the dance area.
+   */
+  public get points(): Map<string, number> {
+    return this._model.points;
+  }
+
+  /**
+   * @returns A DanceAreaMOdel that represents the current state of this DanceAreaController.
+   */
+  public danceAreaModel(): DanceAreaModel {
+    return this._model;
+  }
+
+  /**
+   * Applies updates to this dance area controller's model, setting the music.
+   *
+   * @param updatedModel the model from which we are getting the udpated music
+   */
+  public updateFrom(updatedModel: DanceAreaModel): void {
+    this.music = updatedModel.music;
+  }
+
+  // export function useMusic(controller: DanceAreaController): string {
+  //   const [music, setMusic] = useState(controller.music)
+  // }
+}
