74b5b11128011661f5d22967ae8442ae1329353e
harbourcd <harbour.c@northeastern.neu.edu>
2023-03-21 13:20:47 -0400

    remove unused import

--- townService/src/town/DanceFloorArea.test.ts
+++ townService/src/town/DanceFloorArea.test.ts
@@ -1,6 +1,5 @@
 import { mock, mockClear } from 'jest-mock-extended';
 import { nanoid } from 'nanoid';
-import { readFileSync } from 'fs';
 import Player from '../lib/Player';
 import { getLastEmittedEvent } from '../TestUtils';
 import { TownEmitter } from '../types/CoveyTownSocket';
