e34c13bf9c1b33e15cbec96eba308289bd8110e0
pjanosky <pcjanosky@gmail.com>
2023-03-22 04:02:11 -0400

    layout leaderboard and key viewer components

--- frontend/src/components/Town/TownMap.tsx
+++ frontend/src/components/Town/TownMap.tsx
@@ -6,6 +6,7 @@ import SocialSidebar from '../SocialSidebar/SocialSidebar';
 import NewConversationModal from './interactables/NewCoversationModal';
 import TownGameScene from './TownGameScene';
 import PosterViewerWrapper from './interactables/PosterViewer';
+import DanceOverlayWrapper from './interactables/DanceKeys';
 
 export default function TownMap(): JSX.Element {
   const coveyTownController = useTownController();
@@ -50,7 +51,9 @@ export default function TownMap(): JSX.Element {
     <div id='app-container'>
       <NewConversationModal />
       <PosterViewerWrapper />
-      <div id='map-container' />
+      <div id='map-container'>
+        <DanceOverlayWrapper></DanceOverlayWrapper>
+      </div>
       <div id='social-container'>
         <SocialSidebar />
       </div>
--- frontend/src/components/Town/interactables/DanceKeys.tsx
+++ frontend/src/components/Town/interactables/DanceKeys.tsx
@@ -5,14 +5,68 @@ import useTownController from '../../../hooks/useTownController';
 
 import DanceAreaInteractable from './DanceArea';
 import { DanceMoveResult, NumberKey } from '../../../types/CoveyTownSocket';
-import { useKeyPressed, useKeySequence } from '../../../classes/DanceAreaController';
+import DanceAreaController, {
+  useKeyPressed,
+  useKeySequence,
+} from '../../../classes/DanceAreaController';
+import { Box, Grid, makeStyles, Typography } from '@material-ui/core';
 
 /**
  * DanceKeyViewer displays the keystrokes the user has to press for the current round.
  * It animates a bar that crosses the keys to show the user when to press each key.
  */
-function DanceKeyViewer(): JSX.Element {
-  return <span> dance key viewer here </span>;
+function DanceKeyViewer({
+  danceController,
+}: {
+  danceController: DanceAreaController;
+}): JSX.Element {
+  const useStyles = makeStyles({
+    keyBox: {
+      width: 75,
+      height: 75,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+  });
+  const classes = useStyles();
+  return (
+    <Box width={200} bgcolor='white'>
+      <Typography> Key viewer here</Typography>
+      <Box className={classes.keyBox} bgcolor='purple'>
+        <Typography> 1</Typography>
+      </Box>
+      <Box height={25}></Box>
+      <Box className={classes.keyBox} bgcolor='purple'>
+        <Typography> 2</Typography>
+      </Box>
+      <Box height={25}></Box>
+      <Box className={classes.keyBox} bgcolor='purple'>
+        <Typography> 3</Typography>
+      </Box>
+      <Box height={25}></Box>
+      <Box className={classes.keyBox} bgcolor='purple'>
+        <Typography> 4</Typography>
+      </Box>
+    </Box>
+  );
+}
+
+function DanceLeaderboard({
+  danceController,
+}: {
+  danceController: DanceAreaController;
+}): JSX.Element {
+  return (
+    <Box width={200} bgcolor='white'>
+      <Typography> Leaderboard (this is some mock data)</Typography>
+      <Typography> 1. Slaytie</Typography>
+      <Typography> 2. Slayleen</Typography>
+      <Typography> 3. Slayter</Typography>
+      <Typography> 4. Slayssie</Typography>
+      <Typography> 5. Slaymud</Typography>
+    </Box>
+  );
 }
 
 /**
@@ -21,7 +75,7 @@ function DanceKeyViewer(): JSX.Element {
  *
  * @param props the dance aera interactable that is being interacted with
  */
-export function DanceArea({ danceArea }: { danceArea: DanceAreaInteractable }): JSX.Element {
+export function DanceAreaOverlay({ danceArea }: { danceArea: DanceAreaInteractable }): JSX.Element {
   const townController = useTownController();
   const danceAreaController = useDanceAreaController(danceArea.name);
 
@@ -46,17 +100,35 @@ export function DanceArea({ danceArea }: { danceArea: DanceAreaInteractable }): 
     };
   }, [danceAreaController, townController]);
 
-  return <DanceKeyViewer></DanceKeyViewer>;
+  return (
+    <Grid
+      container
+      justifyContent='space-between'
+      alignItems='flex-end'
+      alignContent='flex-end'
+      style={{ minHeight: '100%' }}>
+      <Grid item>
+        <DanceKeyViewer danceController={danceAreaController}></DanceKeyViewer>
+      </Grid>
+      <Grid item>
+        <DanceLeaderboard danceController={danceAreaController}></DanceLeaderboard>
+      </Grid>
+    </Grid>
+  );
 }
 
 /**
  * The DanceKeysWrapper is suitable to be always rendered inside of a town, and
  * will activate only if the player begins interacting with a dance area.
  */
-export default function DanceKeysWrapper(): JSX.Element {
+export default function DanceOverlayWrapper(): JSX.Element {
   const danceArea = useInteractable<DanceAreaInteractable>('danceArea');
   if (danceArea) {
-    return <DanceArea danceArea={danceArea} />;
+    return (
+      <Box position='absolute' top={0} bottom={0} left={0} right={0}>
+        <DanceAreaOverlay danceArea={danceArea} />
+      </Box>
+    );
   }
   return <></>;
 }
