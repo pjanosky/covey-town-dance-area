9515bd28032a2cf2a0af2d36adc6ada01d61e0ad
harbourcd <harbour.c@northeastern.neu.edu>
2023-04-04 14:08:15 -0400

    fix type issue

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -122,7 +122,8 @@ export interface PosterSessionArea {
 }
 
 /** All the possible number keys that we will detect. */
-export type NumberKey =  'one' | 'two' | 'three' | 'four';
+export type NumberKey =  string;
+//'one' | 'two' | 'three' | 'four';
 
 /**
  * Represents a sequence of keys that the user needs to press to
